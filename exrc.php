8803eaa89c2d088bf0ecb5b6622663f0a3fe682a
Ballout Zakaria <zakariaballou99@gmail.com>
2023-04-28 01:06:12 +0100

    init

---
+++ exrc.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap.css">
+
+</head>
+<body>
+    <center>
+        <form>
+    <div class="1i">
+        Saisir le premier nombre: <br>
+     <input type="text" placeholder="saisir un nombre" size=30><br><br><br>
+     Saisir le deuxiéme nombre: <br>
+     <input type="text" placeholder="saisir un nombre" size=30><br><br><br>
+    </div>
+<div class="dropdown">
+    <label>Choisir votre opération</label><br>
+
+  <select name="op" class="form-control">
+    <option> cliquer ici</option>
+    <option value="-">-</option>
+    <option value="+">+</option>
+    <option value="*">*</option>
+    <option value="%">%</option>
+
+</select><br><br
+</div>
+<div class="butnzac">
+    <input type="submit" class="btn btn-info" value="Calculer">
+    <input type="reset" class="btn btn-info" value="autre calcul">
+</div>
+<div class="smaaaxi">
+
+</div>
+</from>
+</body>
+</html>
