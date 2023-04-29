4414c544ad024676580b633923e2a1a8b6c87ff7
Ballout Zakaria <zakariaballou99@gmail.com>
2023-04-29 02:20:10 +0100

    devbyzac_sara

--- exrc.php
+++ exrc.php
@@ -4,38 +4,77 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>excerice</title>
     <link rel="stylesheet" href="bootstrap.css">
 
 </head>
 <body>
+
+
+    <?php
+
+    $resultat="Calculatrice";
+
+    $num1 =$_GET["P1"];
+
+    $num2 = $_GET["P2"];
+
+    $operation = $_GET["op"];
+
+    if(empty($num1)){
+        $resultat="la zone du premier nombre est vide";
+    }elseif(empty($num2)){
+        $resultat="waaa sir t9 ";
+    }else{
+        if($operation=="-"){
+            $resultat=$num1-$num2;
+        }elseif ($operation=="+"){
+            $resultat=$num1+$num2;
+        }elseif ($operation=="*"){
+            $resultat=$num1*$num2;
+        }elseif ($operation=="%"){
+            $resultat=$num1%$num2;
+        }
+    }
+
+    ?>
+    <form action ="exrc.php" method="get" class="form">
+
     <center>
-        <form>
-    <div class="1i">
-        Saisir le premier nombre: <br>
-     <input type="text" placeholder="saisir un nombreee" size=30><br><br><br>
-     Saisir le deuxiéme nombre: <br>
-     <input type="text" placeholder="saisir un nombreee" size=30><br><br><br>
+       
+    <div class="li">
+       <label >Saisir le premier nombre:</label> 
+     <input type="number" name="P1" class="form-control w-25" placeholder="saisir un nombreee" ><br><br><br>
+     </div>
+     <div class="li">
+     <label >Saisir le deuxiéme nombre:</label>
+     <input type="number" name="P2"  class="form-control w-25" placeholder="saisir un nombreee" ><br><br><br>
     </div>
 <div class="dropdown">
     <label>Choisir votre opération</label><br>
 
-  <select name="op" class="form-control">
+  <select name="op" class="form-control w-25">
     <option> cliquer ici</option>
     <option value="-">-</option>
     <option value="+">+</option>
     <option value="*">*</option>
     <option value="%">%</option>
 
-</select><br><br>
+</select>
+
+<hr>
+
 </div>
 <div class="butnzac">
     <input type="submit" class="btn btn-info" value="Calculer">
     <input type="reset" class="btn btn-info" value="autre calcul">
 </div>
 <div class="smaaaxi">
-
+<?php
+echo $resultat;
+?>
 </div>
-</from>
+</form>
+</div>
 </body>
 </html>
