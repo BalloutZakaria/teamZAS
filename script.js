49c573c81eb3fc5dde42b292e44fd00a1b06fc68
Sarah <badarmy576@gmail.com>
2023-06-20 15:40:01 +0200

    new edit

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ let subMenu = document.getElementById("subMenu");
 function toggleMenu(){
     subMenu.classList.toggle("open-menu");
 }
+
+
+const btns=[
+    {
+        id: 1,
+        name: 'Cars'
+    },
+    {
+        id: 2,
+        name: 'tiyara'
+    },
+    {
+        id: 3,
+        name: 'bateau'
+    },
+    {
+        id: 4,
+        name: 'sperdila'
+    },
+    {
+        id: 5,
+        name: 'm3rftsh'
+    },
+    {
+        id: 6,
+        name: 'shoflhih'
+    },
+]
